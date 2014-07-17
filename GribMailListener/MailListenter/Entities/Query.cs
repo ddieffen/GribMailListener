e4e3551d08eb82ce45140b90bf790f7a7447bece
ddieffen <ddieffen@gmail.com>
2014-07-17 14:29:03 -0500

    Added possibility to download NAM files :)
    At last I can ask my email to send me the weather that I want :)

--- GribMailListener/MailListenter/Entities/Query.cs
+++ GribMailListener/MailListenter/Entities/Query.cs
@@ -5,16 +5,17 @@ using System.Text;
 using System.IO;
 using YellowbrickV6;
 using YellowbrickV6.Entities;
+using System.Net;
 
 namespace MailListenter
 {
     internal class Query
     {
-        internal enum QueryType { saildocs, saildocsanser, forward, help, raceinfo, sectioninfo };
+        internal enum QueryType { saildocs, saildocsanser, forward, help, raceinfo, sectioninfo, grib };
 
-        private AE.Net.Mail.MailMessage m;
-        private QueryType type;
-        private List<string> awaiting = null;
+        internal AE.Net.Mail.MailMessage m;
+        internal QueryType type;
+        internal List<string> awaiting = null;
 
         /// <summary>
         /// Builds a query from a string
@@ -24,6 +25,11 @@ namespace MailListenter
         {
             this.m = message;
             this.awaiting = awaiting;
+        }
+
+        public Query()
+        {
+            // TODO: Complete member initialization
         }
 
         internal bool isValid()
@@ -64,6 +70,11 @@ namespace MailListenter
                     this.type = QueryType.sectioninfo;
                     return true;
                 }
+                if (m != null && m.Subject.StartsWith("nam-conus:"))
+                {
+                    this.type = QueryType.grib;
+                    return true;
+                }
                 return false;
             }
             catch 
@@ -76,118 +87,143 @@ namespace MailListenter
         {
             if (isValid())
             {
-                switch (type) 
+                if (type == QueryType.saildocs)
                 {
-                    case QueryType.saildocs:
-                        SMTPTools.SendMail("query@saildocs.com", m.From.Address, m.Body, false);
-                        return m.From.Address + "," + m.Body;
-                    case QueryType.saildocsanser:
-                        string recipient = "";
-                        string selection = "";
-                        if (this.awaiting != null)
+                    SMTPTools.SendMail("query@saildocs.com", m.From.Address, m.Body, false);
+                    return m.From.Address + "," + m.Body;
+                }
+                else if (type == QueryType.saildocsanser)
+                {
+                    string recipient = "";
+                    string selection = "";
+                    if (this.awaiting != null)
+                    {
+                        foreach (string waiting in awaiting)
                         {
-                            foreach (string waiting in awaiting)
+                            string[] split = waiting.Split(new char[] { ',' }, 2);
+                            if (split[1].Replace("start ", "").Contains(m.Subject))
                             {
-                                string[] split = waiting.Split(new char[] {','}, 2);
-                                if (split[1].Replace("start ","").Contains(m.Subject))
+                                recipient = split[0];
+                                selection = waiting;
+                                break;
+                            }
+                        }
+                        if (!String.IsNullOrEmpty(recipient) && !String.IsNullOrEmpty(selection))
+                        {
+                            List<string> files = new List<string>();
+                            foreach (AE.Net.Mail.Attachment att in m.Attachments)
+                            {
+                                att.Save(att.Filename);
+                                files.Add(att.Filename);
+                            }
+                            SMTPTools.SendMail(recipient, m.Subject, m.Body, false, files.ToArray());
+                            foreach (string file in files)
+                                File.Delete(file);
+                            awaiting.Remove(selection);
+                        }
+                    }
+                }
+                else if (type == QueryType.forward)
+                {
+                    string[] splitForward = m.Subject.Split(new char[] { ':' }, 3);
+                    List<string> filesForward = new List<string>();
+                    foreach (AE.Net.Mail.Attachment att in m.Attachments)
+                    {
+                        att.Save(att.Filename);
+                        filesForward.Add(att.Filename);
+                    }
+                    SMTPTools.SendMail(splitForward[1], "Forwarded from: " + m.From.Address + " => " + splitForward[2], m.Body, true, filesForward.ToArray());
+                    foreach (string file in filesForward)
+                        File.Delete(file);
+                }
+                else if (type == QueryType.raceinfo)
+                {
+                    try
+                    {
+                        string report = "Race name: ";
+                        string[] split = m.Subject.Split(new char[] { ':' }, 2);
+                        Race race = YBTracker.getRaceInformation("http://yb.tl", split[1]);
+                        report += race.title + "\r\n========================\r\n\r\n";
+                        foreach (Tag tag in race.tags)
+                        {
+                            report += tag.id + " - " + tag.name + "\r\n" + "----------------------\r\n";
+                            foreach (Team team in race.teams)
+                            {
+                                if (team.tags.Contains(tag.id))
+                                    report += team.id + " - " + team.name + "\r\n";
+                            }
+                            report += "\r\n";
+                        }
+                        SMTPTools.SendMail(m.From.Address, "Race report for " + split[1], report, false, null);
+                    }
+                    catch
+                    { }
+                }
+                else if (type == QueryType.sectioninfo)
+                {
+                    try
+                    {
+                        string[] split = m.Subject.Split(new char[] { ':' }, 4);
+                        int sectionId = -1; int referenceTeam = -1;
+                        if (Int32.TryParse(split[2], out sectionId)
+                            && Int32.TryParse(split[3], out referenceTeam))
+                        {
+                            Race race = YBTracker.getRaceInformation("http://yb.tl", split[1]);
+                            string tagName = "";
+                            foreach (Tag t in race.tags)
+                            {
+                                if (t.id == sectionId)
                                 {
-                                    recipient = split[0];
-                                    selection = waiting;
+                                    tagName = t.name;
                                     break;
                                 }
                             }
-                            if (!String.IsNullOrEmpty(recipient) && !String.IsNullOrEmpty(selection))
-                            {
-                                List<string> files = new List<string>();
-                                foreach (AE.Net.Mail.Attachment att in m.Attachments)
-                                {
-                                    att.Save(att.Filename);
-                                    files.Add(att.Filename);
-                                }
-                                SMTPTools.SendMail(recipient, m.Subject, m.Body, false, files.ToArray());
-                                foreach (string file in files)
-                                    File.Delete(file);
-                                awaiting.Remove(selection);
-                            }
+                            List<Team> teams = YBTracker.getAllPositions("http://yb.tl", split[1]);
+                            YBTracker.UpdateTeamsMoments(race.teams, teams);
+                            foreach (Team team in race.teams)
+                                YBTracker.UpdateMomentsSpeedHeading(team);
+                            string report = YBTracker.ReportSelectedTeamsHTML(race, sectionId, referenceTeam);
+                            SMTPTools.SendMail(m.From.Address, "Section report for " + tagName, report, true, null);
                         }
-                        break;
-                    case QueryType.forward:
-                        string[] splitForward = m.Subject.Split(new char[] { ':' }, 3);
-                        List<string> filesForward = new List<string>();
-                        foreach (AE.Net.Mail.Attachment att in m.Attachments)
-                        {
-                            att.Save(att.Filename);
-                            filesForward.Add(att.Filename);
-                        }
-                        SMTPTools.SendMail(splitForward[1], "Forwarded from: " + m.From.Address + " => " + splitForward[2], m.Body, true, filesForward.ToArray());
-                        foreach (string file in filesForward)
-                            File.Delete(file);
-                        break;
-                    case QueryType.raceinfo:
-                        try 
-                        {
-                            string report = "Race name: ";
-                            string[] split = m.Subject.Split(new char[]{':'},2);
-                            Race race = YBTracker.getRaceInformation("http://yb.tl", split[1]);
-                            report += race.title + "\r\n========================\r\n\r\n";
-                            foreach (Tag tag in race.tags)
-                            {
-                                report += tag.id + " - " + tag.name + "\r\n" + "----------------------\r\n";
-                                foreach (Team team in race.teams)
-                                { 
-                                    if(team.tags.Contains(tag.id))
-                                        report += team.id + " - " + team.name + "\r\n";
-                                }
-                                report += "\r\n";
-                            }
-                            SMTPTools.SendMail(m.From.Address, "Race report for " + split[1], report, false, null);
-                        }
-                        catch 
-                        { }
-                        break;
-                    case QueryType.sectioninfo:
-                        try
-                        {
-                            string[] split = m.Subject.Split(new char[] { ':' }, 4);
-                            int sectionId = -1; int referenceTeam = -1;
-                            if (Int32.TryParse(split[2], out sectionId)
-                                && Int32.TryParse(split[3], out referenceTeam))
-                            {
-                                Race race = YBTracker.getRaceInformation("http://yb.tl", split[1]);
-                                string tagName = "";
-                                foreach (Tag t in race.tags)
-                                {
-                                    if (t.id == sectionId)
-                                    {
-                                        tagName = t.name;
-                                        break;
-                                    }
-                                }
-                                List<Team> teams = YBTracker.getAllPositions("http://yb.tl", split[1]);
-                                YBTracker.UpdateTeamsMoments(race.teams, teams);
-                                foreach (Team team in race.teams)
-                                    YBTracker.UpdateMomentsSpeedHeading(team);
-                                string report = YBTracker.ReportSelectedTeamsHTML(race, sectionId, referenceTeam);
-                                SMTPTools.SendMail(m.From.Address, "Section report for " + tagName, report, true, null);
-                            }
-                        }
-                        catch
-                        { }
-                        break;
-                    case QueryType.help:
+                    }
+                    catch
+                    { }
+                }
+                else if (type == QueryType.help)
+                {
+                    try
+                    {
                         string message = "This service allows you to: \r\n"
                         + "- Request GRIB files from the saildocs.com service\r\n"
                         + "- Forward an email to one or more recipients from this service \r\n"
                         + "- Query a yellowbrick race \r\n"
                         + "- Results for a section of a yellowbrick race \r\n\r\n"
-                        + "To request a GRIB file, type in the subject 'saildocs:' only, and in the body your query such as 'send coamps:36N,46N,100W,75W' in order to get the weather for lake michigan\r\n\r\n"
+                        + "To request a GRIB file from saildocs service, type in the subject 'saildocs:' only, and in the body your query such as 'send coamps:36N,46N,100W,75W' in order to get the weather for lake michigan\r\n\r\n"
                         + "To request a foreward, type in the subject 'forward:RECIPIENT:SUBJECT' and in the body the body of your message, the message will be delivered to the recipients. If more than one, use comma to separate the email adresses\r\n\r\n"
                         + "To request a yellowbrick race information, put in the subject 'raceinfo:RACE-KEY' where RACE-KEY can be replaced with an existing key\r\n\r\n"
-                        + "To request a report for a yellowbrick race section, put in the subject 'sectioninfo:RACE-KEY:SECTION-ID:REFERENCE-TEAM' where race id is a yellowbrick race id, section is a section id, and reference team is the id of the team used as reference for the report.\r\n\r\n";
+                        + "To request a report for a yellowbrick race section, put in the subject 'sectioninfo:RACE-KEY:SECTION-ID:REFERENCE-TEAM' where race id is a yellowbrick race id, section is a section id, and reference team is the id of the team used as reference for the report.\r\n\r\n"
+                        + "To requast a subset of the NAM CONUS weather model, send a message with a subject like this: nam-conus:-90,-84,47,41:0-12/3,15,18-80/6\r\n\r\n";
                         SMTPTools.SendMail(m.From.Address, "Help Response", message);
-                        break;
-                    default:
-                        break;
+                    }
+                    catch { }
+                }
+                else if (type == QueryType.grib)
+                {
+                    try
+                    {
+                        string[] split = m.Subject.Split(new char[] { ':' }, 2);
+                        if (split[0].ToLower().Equals("nam-conus"))
+                        {
+                            string filename = GRIBTools.DoNam(m.Subject);
+                            List<string> filesForward = new List<string>();
+                            filesForward.Add(filename);
+                            SMTPTools.SendMail(m.From.Address, "NAM-CONUS", "", true, filesForward.ToArray());
+
+                            foreach (string file in filesForward)
+                                File.Delete(file);
+                        }
+                    }
+                    catch { }
                 }
             }
             return "";
---
+++ GribMailListener/MailListenter/Tools/GRIBTools.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace MailListenter
+{
+    public class GribTimeFile
+    {
+        public string model;
+        public int cycle;
+        public int time;
+        public string full_name;
+        public byte[] buffer;
+    }
+
+
+    internal static class GRIBTools
+    {
+
+        internal static List<GribTimeFile> DecomposeFromStrings(List<string> files)
+        {
+            List<GribTimeFile> list = new List<GribTimeFile>();
+
+            foreach (string file in files)
+            {
+                string[] split = file.Split(new char[] { '.' });
+                GribTimeFile tref = new GribTimeFile();
+                tref.full_name = file;
+                tref.cycle = Convert.ToInt16(split[1].Trim('t').Trim('z'));
+                tref.model = split[0];
+                tref.time =  Convert.ToInt16(split[2].TrimStart("awphys".ToCharArray()));
+                list.Add(tref);
+            }
+
+            return list;    
+        }
+
+        /// <summary>
+        /// Parses a string for a NAM-CONUS weather request
+        /// 
+        /// The request must be a string a that forms like this nam-conus:leftlon,rightlon,toplat,bottomlat:0-12/3,15,18-80/6
+        /// Where nam-conus is the name of the model
+        /// Left lon, right lon, top lat, bottom lat are the desired region
+        /// 0-12 indicate that we want all times between 0 to 12 hours after the forecast was issued with a step of 3 hours
+        /// 15 indicates that we also want the forecast for 15 hours
+        /// and 18-80 indicates that we want the forecast for 18 to 80 hours with a step of 6 hours
+        /// </summary>
+        /// <param name="request">Properly formated request</param>
+        internal static string DoNam(string request)
+        {
+            string[] splitRequest = request.Split(new char[] { ':' }, 3);
+            string modelRequest = splitRequest[0];
+            string[] regionRequest = splitRequest[1].Split(new char[] { ',' }, 4);
+            string[] timeRequest = splitRequest[2].Split(new char[] { ',' });
+
+            if (modelRequest != "nam-conus")
+                return "Wrong model";
+            if (regionRequest.Length != 4)
+                return "Wrong number of lat lon";
+
+            int leftLon = (int)Convert.ToDouble(regionRequest[0]);
+            int rightLon = (int)Convert.ToDouble(regionRequest[1]);
+            int topLat = (int)Convert.ToDouble(regionRequest[2]);
+            int bottomLat = (int)Convert.ToDouble(regionRequest[3]);
+
+            string urlDir = "http://nomads.ncep.noaa.gov/cgi-bin/filter_nam.pl";
+            string urlModel = "http://nomads.ncep.noaa.gov/cgi-bin/filter_nam.pl?file=";
+            string urlParameter = "&lev_10_m_above_ground=on&lev_mean_sea_level=on&var_UGRD=on&var_VGRD=on&var_PRMSL=on&subregion=&leftlon=" 
+                + leftLon + "&rightlon=" + rightLon + "&toplat=" + topLat + "&bottomlat=" + bottomLat + "&dir=";
+
+            //finding the index page with all models
+            WebClient w = new WebClient();
+            string s = w.DownloadString(urlDir);
+
+            //find latest simulation day
+            LinkItem latest = Finder.FindLinks(s)[0];
+
+            //find files
+            s = w.DownloadString(latest.Href);
+            List<string> files = Finder.FindOptionsValues(s);
+            List<GribTimeFile> gribTimes = GRIBTools.DecomposeFromStrings(files);
+
+            int maxCycle = 0;
+            foreach (GribTimeFile time in gribTimes)
+                if (time.cycle > maxCycle)
+                    maxCycle = time.cycle;
+
+            //download all binary files
+            List<GribTimeFile> downloaded = new List<GribTimeFile>();
+            int futureStackSize = 0;
+            List<int> cycleTimes = CreateTimes(timeRequest);
+            foreach (GribTimeFile file in gribTimes.Where(t => t.cycle == maxCycle))
+            {
+                if (cycleTimes.Contains(file.time))
+                {
+                    string url = urlModel
+                        + file.full_name
+                        + urlParameter
+                        + latest.Href.Split(new string[] { "dir=" }, StringSplitOptions.None)[1];
+                    HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+                    webRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows 5.1;";
+                    HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+                    Stream responseStream = webResponse.GetResponseStream();
+                    MemoryStream memStream = new MemoryStream();
+                    responseStream.CopyTo(memStream);
+                    byte[] buffer = memStream.ToArray();
+                    file.buffer = buffer;
+                    downloaded.Add(file);
+                    futureStackSize += buffer.Length;
+                }
+            }
+
+            //stacking grib files
+            byte[] stacked = new byte[futureStackSize];
+            int currentPosition = 0;
+            foreach (GribTimeFile grbTime in downloaded)
+            {
+                grbTime.buffer.CopyTo(stacked, currentPosition);
+                currentPosition += grbTime.buffer.Length;
+            }
+
+            //writing the file
+            string path = "nam-" + DateTime.Now.ToString("yyyyMMdd-Hmm") + ".grb";
+            File.WriteAllBytes(path, stacked);
+            return path;
+        }
+
+        private static List<int> CreateTimes(string[] time)
+        {
+            List<int> times = new List<int>();
+            double temp;
+            foreach(string t in time)
+            {
+                if (t.Contains("-"))
+                {//parse interval
+                    int step = 1;
+                    string[] dash = t.Split(new char[]{'-'},2);
+                    int start = (int)Convert.ToDouble(dash[0]);
+                    int finish = 0;
+                    int increment = 1;
+                    if (dash[1].Contains("/"))
+                    {
+                        string[] dashslash = dash[1].Split(new char[] { '/' });
+                        finish = (int)Convert.ToDouble(dashslash[0]);
+                        increment = (int)Convert.ToDouble(dashslash[1]);
+                    }
+                    else
+                        finish = (int)Convert.ToDouble(dash[1]);
+
+                    for (int i = start; i <= finish; i+=increment)
+                    {
+                        times.Add(i);
+                    }
+                }
+                else if (Double.TryParse(t, out temp))
+                {
+                    if (!times.Contains((int)temp))
+                        times.Add((int)temp);
+                }
+
+            }
+            return times;
+        }
+    }
+}
